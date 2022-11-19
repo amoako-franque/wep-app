a68833a371eb12dde7ee8f5fbcce88818a7892ba
armoako <frank.amoako-dev@outlook.com>
2022-11-19 08:38:10 +0000

    refactor

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -15,42 +15,34 @@ const catchError = (err, res) => {
 }
 
 exports.requireSignIn = asyncHandler(async (req, res, next) => {
-  let token
+  const authHeader = req.headers.authorization || req.headers.Authorization
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
-      token = req.headers.authorization.split(" ")[1]
-
-      jwt.verify(token, process.env.JWT_SECRET, async (err, verifiedToken) => {
-        if (err) {
-          return catchError(err, res)
-        }
-
-        const userId = verifiedToken.userId
-        const user = await User.findById(userId).select("-password")
-        if (!user) {
-          return res
-            .status(401)
-            .json({ error: "Unauthorized. Sign in to continue" })
-        }
-        req.user = user
-
-        next()
-      })
-    } catch (error) {
-      console.log(
-        `Error getting user information. Access denied: ${error.message}`
-      )
-      return res.status(401).json({ error: "Access Denied" })
-    }
+  if (!authHeader?.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" })
   }
 
-  if (!token) {
-    return res
-      .status(401)
-      .json({ error: "Access Denied. Please login to continue" })
+  try {
+    const token = authHeader.split(" ")[1]
+
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+      if (err) return catchError(err, res)
+
+      const userId = decoded.userId
+      const user = await User.findById(userId).select("-password")
+
+      if (!user)
+        return res
+          .status(401)
+          .json({ error: "Unauthorized. Sign in to continue" })
+
+      req.user = user
+
+      next()
+    })
+  } catch (error) {
+    console.log(
+      `Error getting user information. Access denied: ${error.message}`
+    )
+    return res.status(401).json({ error: "Access Denied" })
   }
 })
